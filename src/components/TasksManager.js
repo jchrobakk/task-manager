80297245d5bf0f2ca2def0be41a75db5563bc34e
Jakub Chrobak <jchrobakk@gmail.com>
2021-11-08 18:03:27 +0100

    Code form, basic communication with api

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,18 +1,84 @@
-import React from 'react';
+import React from "react";
 
 class TasksManager extends React.Component {
-    state = {
-        tasks: [],
-    }
+  constructor(props) {
+    super(props);
+    this.url = "http://localhost:3005/data";
+  }
+  state = {
+    tasks: [],
+    taskName: "",
+  };
 
-    onClick = () => {
-        const { tasks } = this.state;
-        console.log( tasks)
-    }
+  componentDidMount() {
+    this.loadTasksToState();
+  }
 
-    render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
-    }
+  onClick = () => {
+    const { tasks } = this.state;
+    console.log(tasks);
+  };
+
+  render() {
+    const { taskName } = this.state;
+
+    return (
+      <>
+        <h1>Add new task</h1>
+        <form onSubmit={this.submitHandler}>
+          <label>Task name: </label>
+          <input name="name" value={taskName} onChange={this.changeHandler} />
+          <input type="submit" />
+        </form>
+
+        <main></main>
+      </>
+    );
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+
+    const { taskName } = this.state;
+
+    const data = {
+      name: taskName,
+    };
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch(this.url, options)
+      .then((resp) => console.log(resp))
+      .catch((err) => console.error(err))
+      .finally(() => {
+        this.setState({
+          taskName: "",
+        });
+
+        this.loadTasksToState();
+      });
+  };
+
+  changeHandler = (e) => {
+    const { value } = e.target;
+    this.setState({
+      taskName: value,
+    });
+  };
+
+  loadTasksToState = () => {
+    fetch(this.url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({
+          tasks: [...data],
+        });
+      });
+  };
 }
 
 export default TasksManager;
