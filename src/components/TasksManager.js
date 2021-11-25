77a70680f5e86bbde0a759e5a3dc4052dc4dc38b
Jakub Chrobak <jchrobakk@gmail.com>
2021-11-25 15:58:06 +0100

    Code starting, stoping and finish tasks

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -4,6 +4,7 @@ class TasksManager extends React.Component {
   constructor(props) {
     super(props);
     this.url = "http://localhost:3005/data";
+    this.idInterval = null;
   }
   state = {
     tasks: [],
@@ -50,6 +51,10 @@ class TasksManager extends React.Component {
         isRemoved: false,
       };
 
+      this.setState({
+        tasks: [...this.state.tasks, data],
+      });
+
       const options = {
         method: "POST",
         body: JSON.stringify(data),
@@ -63,8 +68,6 @@ class TasksManager extends React.Component {
           this.setState({
             taskName: "",
           });
-
-          this.loadTasksToState();
         });
     } else {
       alert("Enter task name");
@@ -88,22 +91,36 @@ class TasksManager extends React.Component {
       });
   };
 
+  isButtonStartStopDisabled(task) {
+    return (this.idInterval && task.isRunning === false) || task.isDone
+      ? true
+      : false;
+  }
+
   renderTasksList() {
     const tasksList = this.state.tasks;
-    const taskListElement = tasksList.map((task) => {
+    const sortedTasksList = tasksList.sort((task) => task.isDone);
+    const taskListElement = sortedTasksList.map((task) => {
       return (
-        <section className="task">
+        <section className={task.isDone ? "task task--done" : "task"}>
           <header className="task__header">
             {task.name} {task.time}
           </header>
           <footer className="task__footer">
             <button
-              onClick={() => this.toggleStartStop(task.id)}
+              onClick={() => this.handleClick(task)}
               className="task__startstop"
+              disabled={this.isButtonStartStopDisabled(task)}
             >
               start/stop
             </button>
-            <button className="task__finished">finished</button>
+            <button
+              className="task__finished"
+              onClick={() => this.handleFinish(task)}
+              disabled={task.isDone}
+            >
+              finished
+            </button>
             <button className="task__delete" disabled="true">
               delete
             </button>
@@ -115,74 +132,73 @@ class TasksManager extends React.Component {
     return taskListElement;
   }
 
-  toggleStartStop = (id) => {
-    let isAnyTaskRunning;
-    const { tasks } = this.state;
-    if (tasks.some((e) => e.isRunning === true)) {
-      isAnyTaskRunning = true;
-    } else {
-      isAnyTaskRunning = false;
-    }
-
-    if (isAnyTaskRunning) {
-      this.toggleIsRunning(id);
-      console.log("Task is running");
-      console.log(tasks);
-    } else {
-      this.toggleIsRunning(id);
-      console.log("No task is running");
-      console.log(tasks);
-    }
-  };
-
-  toggleIsRunning = (id) => {
-    const { tasks } = this.state;
-    const task = tasks.find((task) => task.id === id);
-    const data = {
-      ...task,
-      isRunning: !task.isRunning,
-    };
-
-    const options = {
-      method: "PUT",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    };
-
-    fetch(`${this.url}/${id}`, options)
-      .then((resp) => console.log(resp))
-      .catch((err) => console.error(err))
-      .finally(() => {
-        this.loadTasksToState();
-      });
+  handleClick = (task) => {
+    task.isRunning ? this.stopTask(task.id) : this.startTask(task.id);
   };
 
   startTask = (id) => {
-    setInterval(() => {
+    this.idInterval = setInterval(() => {
       this.incrementTime(id);
     }, 1000);
   };
 
-  incrementTime = (id) => {
-    const { tasks } = this.state;
-    const task = tasks.find((task) => task.id === id);
-    const data = {
-      ...task,
-      time: task.time + 1,
-    };
+  stopTask = (id) => {
+    clearInterval(this.idInterval);
+    this.idInterval = null;
 
+    const newTasks = this.state.tasks.map((t) => {
+      if (t.id === id) {
+        const task = { ...t, isRunning: false };
+        this.updateTaskInApi(task);
+        return task;
+      }
+
+      return t;
+    });
+
+    this.setState({ tasks: newTasks });
+  };
+
+  updateTaskInApi = (task) => {
     const options = {
       method: "PUT",
-      body: JSON.stringify(data),
+      body: JSON.stringify(task),
       headers: { "Content-Type": "application/json" },
     };
 
-    fetch(`${this.url}/${id}`, options)
+    fetch(`${this.url}/${task.id}`, options)
       .then((resp) => console.log(resp))
-      .catch((err) => console.error(err))
-      .finally(() => {
-        this.loadTasksToState();
-      });
+      .catch((err) => console.error(err));
+  };
+
+  incrementTime = (id) => {
+    const newTasks = this.state.tasks.map((t) => {
+      if (t.id === id) {
+        const task = { ...t, isRunning: true, time: t.time + 1 };
+        this.updateTaskInApi(task);
+        return task;
+      }
+
+      return t;
+    });
+
+    this.setState({ tasks: newTasks });
+  };
+
+  handleFinish = (task) => {
+    this.stopTask(task.id);
+
+    const newTasks = this.state.tasks.map((t) => {
+      if (t.id === task.id) {
+        const task = { ...t, isDone: true };
+        this.updateTaskInApi(task);
+        return task;
+      }
+
+      return t;
+    });
+
+    this.setState({ tasks: newTasks });
   };
 }
 
